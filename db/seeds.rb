240629b049a8d8ecb2e777be6755f64a5a6612e3
Nirav <niyando@gmail.com>
2018-12-31 13:45:42 +0530

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create do |u|
+  u.name = Faker::Name.name
+  u.email = "niyando@gmail.com"
+  u.password = "password123"
+end
+
+user_cart = user.create_cart
+
+# 20.times do |n|
+#   Item.create do |i|
+#     i.name = Faker::Food.dish
+#     i.description = Faker::Food.description
+#     i.amount = (5..20).to_a.sample
+#   end
+# end
+
+3.times do |n|
+  user_cart.cart_items.create(item: Item.all.sample, quantity: 1)
+end
+
