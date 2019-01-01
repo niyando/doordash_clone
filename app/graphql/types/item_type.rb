7b433c8cdc7b5cab05f67569207319329cc00ee8
Nirav <niyando@gmail.com>
2019-01-01 11:04:04 +0530

    endpoint to get info about single item

--- app/graphql/types/item_type.rb
+++ app/graphql/types/item_type.rb
@@ -3,5 +3,6 @@ module Types
     field :id, ID, null: false
     field :name, String, null: false
     field :description, String, null: true
+    field :amount, Float, null: false
   end
 end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,9 +4,16 @@ module Types
     field :items, [ItemType], null: true do
       description "gets all items"
     end
-  
     def items
       Item.all
+    end
+
+    field :item, ItemType, null: true do
+      description "gets the requested item"
+      argument :id, ID, required: true
+    end
+    def item(params)
+      Item.find(params[:id])
     end
     
   end
