6b710e5e2b40d8cc715b727db86f9c50b25f4916
Nirav <niyando@gmail.com>
2019-01-01 11:14:43 +0530

    ability to search and paginate

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,17 +3,22 @@ module Types
     
     field :items, [ItemType], null: true do
       description "gets all items"
+      argument :offset, Int, required: false, default_value: 0
+      argument :keywords, String, required: false, default_value: nil
     end
-    def items
-      Item.all
+    def items(args)
+      Item
+        .search(args[:keywords])
+        .paginate(args[:offset])
+        .order(created_at: :desc)
     end
 
     field :item, ItemType, null: true do
       description "gets the requested item"
       argument :id, ID, required: true
     end
-    def item(params)
-      Item.find(params[:id])
+    def item(args)
+      Item.find(args[:id])
     end
     
   end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,19 @@
 class Item < ApplicationRecord
   validates_presence_of :name, :amount
+
+  cattr_accessor(:paginates_per) { 10 }
+
+  def self.paginate(offset)
+    offset(offset).limit(self.paginates_per)
+  end
+
+  def self.search(keywords)
+    return self if keywords.blank?
+    where('lower(name) like :keywords OR lower(description) like :keywords', :keywords => "%#{keywords.downcase}%")
+  end
+
+  def abbr_description
+    self.description.truncate(180)
+  end
   
 end
