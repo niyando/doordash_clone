53761ff45c16b11f64124982205d1b966d95bc97
Nirav <niyando@gmail.com>
2019-01-06 23:18:05 +0530

    add user id

--- app/graphql/types/group_cart_item_type.rb
+++ app/graphql/types/group_cart_item_type.rb
@@ -1,5 +1,6 @@
 module Types
   class GroupCartItemType < GraphQL::Schema::Object
+    field :user_id, ID, null: false
     field :user, UserType, null: false
     field :item, ItemType, null: false
     field :quantity, Int, null: false
