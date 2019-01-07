b2da28b4e86680933370466bc1341a85fd186cc3
Nirav <niyando@gmail.com>
2019-01-07 16:57:53 +0530

    adds depedent destroy to carts

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,6 @@
 class Cart < ApplicationRecord
   belongs_to :user
-  has_many :cart_items
+  has_many :cart_items, dependent: :destroy
   has_many :items, through: :cart_items
   
 end
--- app/models/group_cart.rb
+++ app/models/group_cart.rb
@@ -1,6 +1,6 @@
 class GroupCart < ApplicationRecord
   belongs_to :user
-  has_many :group_cart_items
+  has_many :group_cart_items, dependent: :destroy
   has_many :items, through: :group_cart_items
 
   has_secure_token
